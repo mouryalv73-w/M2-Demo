176fc204d6dbd97cf4b869df9e1399f93df3f19c
Tejas Mourya <Tejas Mourya@TejasMourya>
2026-03-12 12:24:02 +0530

    commit from AutomationEngineer workspace

--- src/test/java/practice/Demo2.java
+++ src/test/java/practice/Demo2.java
@@ -4,7 +4,7 @@ public class Demo2
 {
 	public static void main(String[] args) {
 		System.out.println("Practicing pull operations");
-		System.out.println("Practicing git conflit from FrameworkEngineer workspace");
+		System.out.println("Practicing git conflit from AutomationEngineer workspace");
 		
 	}
 
