8e5e686ebff524460a46e9eea6c37b7f2d66bf85
Tejas Mourya <Tejas Mourya@TejasMourya>
2026-03-12 12:10:25 +0530

    commit from FrameworkEngineer workdspace

--- src/test/java/practice/Demo2.java
+++ src/test/java/practice/Demo2.java
@@ -4,6 +4,7 @@ public class Demo2
 {
 	public static void main(String[] args) {
 		System.out.println("Practicing pull operations");
+		System.out.println("Practicing git conflit from FrameworkEngineer workspace");
 		
 	}
 
